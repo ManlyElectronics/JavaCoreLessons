02dd8a3da375de8b5b5fbcc2d0b120e93077fc44
DimitriRytsk <sales@manlyelectronics.com.au>
2020-11-28 22:19:04 +1100

    Update Task2.java
    непонял смысл игры

--- lessons/src/lesson3/homework/Task2.java
+++ lessons/src/lesson3/homework/Task2.java
@@ -1,12 +1,12 @@
 package lesson3.homework;
-//Создать массив из слов // * Сделанно
-// слово = случайно // * Сделанно
-// * Сделать
-// цикл 1: i< длинна слова.
+//Создать массив из слов // Сделанно
+// слово = случайно // Сделанно
+// цикл пока не угадали // Сделано
 // ответ пользователя // Сделано
 // ответ == слово ? // Сделано
 //   да -> угадали ! // Сделано
-//   нет -> нет - > показывается i букв из слова + ..   -> цикл 1
+// * Сделать
+//   нет -> нет - > показывается i букв из слова + ..   -> цикл
 //Играем до тех пор, пока игрок не отгадает слово. Используем только маленькие буквы.
 
 //String str = "apple"; char a = str.charAt(0); - метод, вернет char, который стоит в слове str на первой позиции
@@ -16,15 +16,19 @@ import java.util.Scanner;
 
 public class Task2 {
     public static Scanner sc =new Scanner(System.in);
-    public static void main(String[] args) { String[] words = new String[]{ "apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi",
-                "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato" };
-    String message = "угадайте слово";
-    String word = RandomWordFromArr(words);
-    System.out.println(word);// тест подсказка
-    while (!message.equals(word)){
-        message = AsksString(message);
-        }
-    System.out.println("Угадали!");
+    public static void main(String[] args) {
+        String[] words = new String[]{"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi",
+                "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        String message = "угадайте слово";
+        String word = RandomWordFromArr(words);
+        System.out.println(word);// тест подсказка
+        int i = 0;
+        String answer;
+        do {
+            answer = AsksString(message);
+            System.out.println("буква номер " + (answer.length()-1) + " "+ word.charAt(answer.length()-1));
+        } while (!answer.equals(word));
+        System.out.println("Угадали!");
     }
 
     private static String RandomWordFromArr(String... A) {
