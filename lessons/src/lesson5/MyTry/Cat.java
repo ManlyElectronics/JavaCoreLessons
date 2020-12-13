cdcadff6039ceaa2d2dad2b5aee4ab88af553e29
DimitriRytsk <sales@manlyelectronics.com.au>
2020-12-13 16:54:33 +1100

    trys

--- lessons/src/lesson5/MyTry/Cat.java
+++ lessons/src/lesson5/MyTry/Cat.java
@@ -2,20 +2,26 @@ package lesson5.MyTry;
 
 public class Cat {
 protected String Имя = "Имя по умолчанию";
-protected String Фамилия = "Фамилия по умолчанию";
-protected String Отчество = "Отчество по умолчанию";
 protected int Возраст =1;
 
-public Cat (String name, String surname, String patronymic, int age){
-    // this.Имя = name; // если не определять здесь берётся по умолчанию
-    this.Фамилия = surname;
-    this.Отчество = patronymic;
+
+   public Cat (String name, int age){
+    this.Имя = name; // если не определять здесь берётся по умолчанию
     this.Возраст = age;
 // Зачем задавать параметры через метод ?
 }
 
-    public Cat() {
+   public Cat() {
       //  String name = "неизвестное имя";
-        System.out.println("Кот безимянный");
+        System.out.println("подобрал безимянного кота");
+    }
+    public void printInfo(){ System.out.println("Имя: " + this.Имя + "  Возраст: " + this.Возраст); }
+    public void info(){
+       System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+         return "Имя: " + this.Имя + "  Возраст: " + this.Возраст;
     }
 }
