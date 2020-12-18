57ac33a94d687c5a3a5a76c912f89948d223b706
DimitriRytsk <sales@manlyelectronics.com.au>
2020-12-18 20:34:55 +1100

    cleaned code in TicTacToe

--- lessons/src/lesson4/homework/TicTacToe.java
+++ lessons/src/lesson4/homework/TicTacToe.java
@@ -1,13 +1,14 @@
 package lesson4.homework;
 // Крестики нолики. проверка победы путём сравнения соседних элементов и сщётчика. также проверяется произвольные диагонали данной длинны со здвигом вверх вних по Y
 // ученик Димитрий Рыцк
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
     //Создание игрового поля.
     public static char[][] map;
-    public static final int SIZE = 10; //размер поля
+    public static final int SIZE = 9; //размер поля, проверенно до размера 10 000 x 10 000 но от 10 не влезают шапки
     public static final int DOTS_TO_WIN = 3; // нужно для победы
 
     //Ячейки поля.
@@ -44,7 +45,8 @@ public class TicTacToe {
         }
         System.out.println("Игра закончена");
     }
-//заполнение символами пустоты.
+
+    //заполнение символами пустоты.
     public static void initMap() {
         map = new char[SIZE][SIZE];
         for (int i = 0; i < SIZE; i++) {
@@ -53,7 +55,8 @@ public class TicTacToe {
             }
         }
     }
-//Вывод поля. Первый цикл i печатает шапку игрового поля. внутренний j цикл отвечает за печать одной строки, после одной строки перевод каретки на следующую строку System.out.println(). После этого счетчик i увеличивается и производится печать следующих строк. После распечатки всего поля ставится дополнительная пустая строка для разделения выводов полей.
+
+    //Вывод поля. Первый цикл i печатает шапку игрового поля. внутренний j цикл отвечает за печать одной строки, после одной строки перевод каретки на следующую строку System.out.println(). После этого счетчик i увеличивается и производится печать следующих строк. После распечатки всего поля ставится дополнительная пустая строка для разделения выводов полей.
     public static void printMap() {
         for (int i = 0; i <= SIZE; i++) {
             System.out.print(i + " ");
@@ -68,7 +71,8 @@ public class TicTacToe {
         }
         System.out.println();
     }
-//Ход человека.
+
+    //Ход человека.
     public static void humanTurn() {
         int x, y;
         do {
@@ -78,13 +82,15 @@ public class TicTacToe {
         } while (!isCellValidAndEmpty(x, y));
         map[x][y] = DOT_X;
     }
-//Проверка возможности установки фишки в указанную ячейку. Если координаты указывают на занятое поле, либо находятся за его пределами возвращаем false, в противном случае – true.
+
+    //Проверка возможности установки фишки в указанную ячейку. Если координаты указывают на занятое поле, либо находятся за его пределами возвращаем false, в противном случае – true.
     public static boolean isCellValidAndEmpty(int x, int y) {
         if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
         if (map[x][y] == DOT_EMPTY) return true;
         return false;
     }
-//Ход компьютера. rand.nextInt(n) возвращает случайное число в диапазоне от 0 до n – 1 включительно.
+
+    //Ход компьютера. rand.nextInt(n) возвращает случайное число в диапазоне от 0 до n – 1 включительно.
     public static void aiTurn() {
         int x, y;
         do {
@@ -94,55 +100,59 @@ public class TicTacToe {
         System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
         map[x][y] = DOT_O;
     }
-// Собственно ДЗ
+
+    // Собственно ДЗ
 //Проверка победы по горизонталям, вертикалям и диагоналям зделанна сравнением соседних элементов и счёчиком n. Если равны сщётчик увеливается. если нет устанавливается снова в 1.
 // для упращения, границы сканирования пренебрегаются, и проверка валидности делается внутри areCellsEqual(соседние координаты)
 // включая со здвигом по диагоналям при размере больше 3
     public static boolean checkWin() {
         // проверка по строкам
         int n = 1;
-        for (int y = 0; y < SIZE ; y++) {
-             for (int x = 0; x < SIZE; x++) {
-                    if (areCellsEqual(x,y,x+1,y)) n++;
-                    else n = 1;
-                    if (n == DOTS_TO_WIN) return true;
+        for (int y = 0; y < SIZE; y++) {
+            for (int x = 0; x < SIZE; x++) {
+                if (areCellsEqual(x, y, x + 1, y)) n++;
+                else n = 1;
+                if (n == DOTS_TO_WIN) return true;
             }
         }
         // по столбцам
-        for (int x = 0; x < SIZE ; x++) {
+        for (int x = 0; x < SIZE; x++) {
             for (int y = 0; y < SIZE; y++) {
-                if (areCellsEqual(x,y,x,y+1)) n++;
+                if (areCellsEqual(x, y, x, y + 1)) n++;
                 else n = 1;
                 if (n == DOTS_TO_WIN) return true;
             }
         }
-        // по диагонали и со сдвигом d по дигонали вверх вниз по Y
-        for (int d = - SIZE; d < SIZE; d++) {
-        for (int i= 0; i < SIZE ; i++) {
-            if (areCellsEqual(i,i-d,i+1,i+1-d)) n++;
-                else n = 1;
-                if (n == DOTS_TO_WIN) return true;
-            }
-        }
-        // по побочной диагонали и со сдвигом d дигонали вверх вних по Y
+        // по всем возможным основным (нисходящим) диагоналям включая со сдвигом d вверх вниз по Y
         for (int d = -SIZE; d < SIZE; d++) {
-        for (int i= 0; i < SIZE ; i++) {
-            if (areCellsEqual(i,SIZE-1-i-d,i+1,SIZE-i-2-d)) n++;
-            else n = 1;
-            if (n == DOTS_TO_WIN) return true;
+            for (int i = 0; i < SIZE; i++) {
+                if (areCellsEqual(i, i - d, i + 1, i + 1 - d)) n++;
+                else n = 1;
+                if (n == DOTS_TO_WIN) return true;
+            }
         }
+        // по всем возможным побочным (восходящим) диагоналям включая со сдвигом d вверх вних по Y
+        for (int d = -SIZE; d < SIZE; d++) {
+            for (int i = 0; i < SIZE; i++) {
+                if (areCellsEqual(i, SIZE - 1 - i - d, i + 1, SIZE - i - 2 - d)) n++;
+                else n = 1;
+                if (n == DOTS_TO_WIN) return true;
+            }
         }
         return false;
     }
+
     // элемент существует и уже схожен ?
     public static boolean isCellValidAndNotEmpty(int x, int y) {
         return !(x < 0 || x >= SIZE || y < 0 || y >= SIZE || (map[x][y] == DOT_EMPTY));
     }
+
     // соседние элементы существуют схожены и равны ?
-public static boolean areCellsEqual(int x, int y, int x1, int y1){
-    if (!isCellValidAndNotEmpty(x,y) || !isCellValidAndNotEmpty(x1,y1)) return false;
-     return map[x][y] == map[x1][y1];
-}
+    public static boolean areCellsEqual(int x, int y, int x1, int y1) {
+        if (!isCellValidAndNotEmpty(x, y) || !isCellValidAndNotEmpty(x1, y1)) return false;
+        return map[x][y] == map[x1][y1];
+    }
+
     public static boolean isMapFull() {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
