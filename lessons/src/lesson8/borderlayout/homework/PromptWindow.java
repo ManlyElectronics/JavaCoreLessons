cc7220eb958b9fecd00fa0f6beda623297434326
DimitriRytsk <sales@manlyelectronics.com.au>
2020-12-23 21:14:42 +1100

    HW V1

---
+++ lessons/src/lesson8/borderlayout/homework/PromptWindow.java
@@ -0,0 +1,34 @@
+package lesson8.borderlayout.homework;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PromptWindow extends JFrame {
+
+    public static String output;
+
+    public PromptWindow() {
+
+        setBounds(400, 350, 100, 100);
+        setTitle("Demo");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JTextField field = new JTextField();
+        add(field);
+        field.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                System.out.println("Your message: " + field.getText());
+                output = field.getText();
+
+            }
+        });
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new PromptWindow();
+    }
+}
+
